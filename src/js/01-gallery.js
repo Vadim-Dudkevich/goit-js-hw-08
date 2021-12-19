266da83f5f61f66437c46fec8fc683751502ceb9
Vadim Dudkevich <justin_duda@ukr.net>
2021-12-19 14:32:23 +0200

    add code
    add code for 01-gallery.js
    SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemMarkup());
+
+console.log(galleryContainer);
+
+function galleryItemMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class = "gallery__item">
+    <a class = "gallery__link" href = "${original}">
+    <img
+    class = "gallery__image"
+    src = "${preview}"
+    data-source = "${original}"
+    alt = "${description}"
+    />
+    </a>
+    </div>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionType: 'alt',
+});
 
 console.log(galleryItems);
